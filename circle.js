cfa26b81f1f306b4367577739a9ec8b624f77ed1
Mindy Miller <mmillerks@gmail.com>
2022-02-14 18:58:55 -0600

    second commit

---
+++ circle.js
@@ -0,0 +1,14 @@
+//PRACTICE MODULE #2 (need to finish)
+const area = (rad) => {
+    return  Math.PI * (rad * rad)
+}
+
+const circumference = () => {
+    return Math.PI * rad * 2
+}
+
+
+module.exports = {
+    area,
+    circumference
+}
---
+++ random.js
@@ -0,0 +1,8 @@
+
+//PRACTICE MODULE #1
+const random = (min, max) => {
+    return Math.floor(Math.random() * (max-min) + min)
+}
+
+module.exports = random;
+
